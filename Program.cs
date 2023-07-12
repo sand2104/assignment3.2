094a08ecfc8035673bb243edca3323f4277e71b4
sand2104 <prasadsandy2000@gmail.com>
2023-07-12 15:40:08 +0530

    assignment3.2

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment3._2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Convert string to integer using TryParse
+            Console.Write("Enter an integer: ");
+            string intInput = Console.ReadLine();
+
+            if (int.TryParse(intInput, out int intValue))
+            {
+                int multipliedValue = intValue * 5;
+                Console.WriteLine("Conversion successful! Multiplied value: " + multipliedValue);
+            }
+            else
+            {
+                Console.WriteLine("Conversion to integer failed. Please enter a valid integer.");
+            }
+
+            // Convert string to DateTime using Convert
+            Console.Write("Enter a date and time (yyyy-MM-dd HH:mm:ss): ");
+            string dateTimeInput = Console.ReadLine();
+
+            try
+            {
+                DateTime convertedDateTime = Convert.ToDateTime(dateTimeInput);
+                DateTime subtractedDateTime = convertedDateTime.AddMonths(-1);
+                Console.WriteLine("Conversion successful! Subtracted 1 month: " + subtractedDateTime);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Conversion to DateTime failed. Please enter a valid date and time format.");
+            }
+
+            // Convert string to TimeSpan using Parse
+            Console.Write("Enter a time span (hh:mm:ss): ");
+            string timeSpanInput = Console.ReadLine();
+
+            try
+            {
+                TimeSpan parsedTimeSpan = TimeSpan.Parse(timeSpanInput);
+                TimeSpan addedTimeSpan = parsedTimeSpan.Add(new TimeSpan(2, 0, 0));
+                Console.WriteLine("Conversion successful! Added 2 hours: " + addedTimeSpan);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Conversion to TimeSpan failed. Please enter a valid time span format.");
+            }
+        }
+    }
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("assignment3.2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("assignment3.2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c40d2030-4aee-4834-9dec-5c6935c82d7c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
